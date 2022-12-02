085a89939ed3aeb9ff8dd06392cb573218a89a41
abugaedd <abugaeddie@gmail.com>
2022-12-01 18:13:23 -0800

    Import add function

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+    Args:
+    a: First Int
+    b: Second Int
+    Returns:
+    The return value: a + b
+    """
+    return (a + b)
