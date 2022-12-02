9551c3f00deec948c924d2d4b89b8e48d60a85aa
abugaedd <abugaeddie@gmail.com>
2022-12-02 13:34:17 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     if count == 1:
         print("0 arguments.")
     elif count == 2:
-        print("{:d} argument:".format(count -1))
+        print("{:d} argument:".format(count - 1))
     else:
-        print("{:d} arguments:".format(count -1))
+        print("{:d} arguments:".format(count - 1))
 
     for i in range(1, count):
         print("{:d}: {:s}".format((i), argv[i]))
